46e73353b6b1fc209f4939b9acd64932662986ea
Divyaja Kakarla <divyakakarla93@gmail.com>
2020-09-16 23:35:26 +0530

    IOC Comment Update

--- gitRepo/SDivIOC/src/com/dk/sd/Mobile.java
+++ gitRepo/SDivIOC/src/com/dk/sd/Mobile.java
@@ -6,9 +6,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Mobile {
 	public static void main(String[] args) {
 		
+		
+		
 		ApplicationContext context  = new ClassPathXmlApplicationContext("beans.xml");
 		System.out.println("config loaded");
-		
+		//Using interface ref to access Network Details 
 		Sim sim = context.getBean("sim",Sim.class);
 		sim.calling();
 		sim.data();
